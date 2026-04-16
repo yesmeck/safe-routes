16aa37f9ed43083bcc34466174fea7dcf2151462
Wei Zhu <yesmeck@gmail.com>
2026-04-16 21:33:32 +0930

    feat: add vite 8 support (#159)
    BREAKING CHANGE: This version supports vite 8 only

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,16 +1,18 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/postcss";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { safeRoutes } from 'safe-routes/vite';
+import { safeRoutes } from "safe-routes/vite";
 
 export default defineConfig({
   base: "/blog",
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths(), safeRoutes()],
+  plugins: [reactRouter(), safeRoutes()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
